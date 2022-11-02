479fec41b69690c5bc6de72775ffc443c75b194c
Yannik <ypsi.hoffmann@gmail.com>
2022-11-02 20:58:08 +0100

    Made code a little easier to read. Side note: Deltax and Deltay were already globally defined consts

--- ModelsAndFunctions.hpp
+++ ModelsAndFunctions.hpp
@@ -25,27 +25,18 @@ const double Deltay = 0.0;
 // MODEL NAMESPACES //
 
 namespace GBWModel {
-
+    
     double T(double bx, double by) {
-
         return exp( -(std::pow(bx, 2.0)+std::pow(by, 2.0))/2.0/std::pow(R, 2.0) );
-
     }
-
 
     double Q_s(double bx, double by) {
-
         return C * T(bx, by);
-
     }
-
 
     double dsigma_qq_d2b(double bx, double by, double rx, double ry) {
-
         return 2.0 * ( 1 - exp(-0.25*std::pow(Q_s(bx, by), 2.0) * (std::pow(rx, 2.0)+std::pow(ry, 2.0))) );
-
     }
-
 }
 
 
@@ -53,18 +44,12 @@ namespace GBWModel {
 namespace PsiPsiSimpleModel {
 
     double PsiPsi_T_no_delta(double Q_squared, double rx, double ry, double z) {
-
         if (rx == 0.0) {rx = 1e-30;} // <-- without this GSL produces an error because Suave evaluates at xx=0 and gsl_sf_bessel_Kn(0,x) is only defined for x>0
         if (ry == 0.0) {ry = 1e-30;}
         return -A_Q * std::pow(2.0*m_Q_c*N_c, 1.0/2.0) * e_Q * e *  gsl_sf_bessel_Kn(0, epsilon * std::pow((std::pow(rx,2.0)+std::pow(ry,2.0)),0.5));
-
     }
-
 
     double PsiPsi_L_no_delta(double Q_squared, double rx, double ry, double z) {
-
         return 2.0 / m_Q_c * Q_squared * z * (1.0-z) * PsiPsi_T_no_delta(Q_squared,rx,ry,z);
-
     }
-
 }
